2f6471dce84d8690916aec30f3fc44ec2d5f3009
MMauriciomt24 <mm.mendez.t@edu.utc.mx>
2021-11-29 22:32:42 -0600

    SumaDosNumeros.java

---
+++ 03_Suma2Numeros/SumaDosNumeros.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Sumadosnumeros{
+    public static void main(String[] args){
+        try (Scanner entrada = nuevo) {
+            Scanner(Sistema. en);
+            int n,r=0;
+            hacer{
+                Sistema. fuera. println("Ingrese numero a sumar, ingresa 0 para dejar de sumar: " );
+ n=entrada. nextInt();
+                r=r+n;
+            }
+        } mientras (n != 0);
+        Sistema. fuera. println("La suma de los numeros es "+ r);
+    }
+}
